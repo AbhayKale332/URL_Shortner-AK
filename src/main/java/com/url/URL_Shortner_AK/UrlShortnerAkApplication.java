ddd3bee7ed11f34b192dd6ac116d72be04a25963
AbhayKale <abudada1209@gmail.com>
2026-02-18 18:50:32 +0530

    Hii

---
+++ src/main/java/com/url/URL_Shortner_AK/UrlShortnerAkApplication.java
@@ -0,0 +1,13 @@
+package com.url.URL_Shortner_AK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerAkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerAkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/url/URL_Shortner_AK/UrlShortnerAkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.URL_Shortner_AK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerAkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
